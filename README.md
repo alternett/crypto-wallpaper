a721a97a237edbdb4a33f919a9b40e079e185d64
andreas <ligma.balls69420weed@gmail.com>
2021-04-07 20:59:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+## Info
+A fairly simple Python script that uses the [CoinGecko API](https://www.coingecko.com/en/api) to replace your desktop wallpaper with a price chart of your favorite cryptocurrency. The script is also extremely easy to modify to your needs, feel free to do so! If you have any problems, feel free to send me a Reddit DM or open an issue (or open a pull request if you can fix it yourself)!
+
+If you want to donate:
+
+BTC - `bc1qlxx5kqec6ly669lnpkl6nfq6xrvrrmakzmnlrd`
+
+ETH - `0x4032c5AE16C49b5C9A1756c9D98Ef560Fa559ae3`
+
+XMR - `85xwVYAVSrnXACsFbf5Gzs9vLLeVux2AyNXiHh8J77UHctb55j9aYwQ9ESfm9ispnDWDChD4op3RgBb62u8devwyB8csvTG`
+
+## Installation
+1. Python is required, if you don't already have it installed, download and install it from [here](https://www.python.org/downloads/).
+2. Clone this project by running `git clone https://github.com/alternett/crypto-wallpaper`
+3. Install the required packages by running `py -m pip install -r requirements.txt`
+   
+   * If `py` is not recognized as a command try replacing it with either `python` or `python3`, or add it to you PATH.
+   
+   * Some people may have some issues with matplotlib, try installing it by running `py -m pip install matplotlib` separately.
+
+4. Modify the [config](#config) to your needs.
+5. Run the program with `py main.py`
+
+## Config
+Setting          | Description
+-----------------|------------
+coin             | The ID of the coin you want to display. [Here's the full list](https://api.coingecko.com/api/v3/coins/list).
+currency         | The currency you want to use. [Here's a list](https://api.coingecko.com/api/v3/simple/supported_vs_currencies) of supported currencies.
+timespan         | The timespan of the graph, measured in hours. Write 0 or "max" to show an all-time graph.
+refresh_interval | How often it will request info from the API (in seconds). PS: CoinGecko only update their data every few minutes, that's why you may not notice changes by lowering this.
+color            | The color of the graph. You can see all colors [here](https://matplotlib.org/stable/gallery/color/named_colors.html) or by [scrolling down](#colors).
+dark_theme       | Toggles between dark theme (true) or light theme (false).
+figsize          | [The size of the image in inches](https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.figure.html). Change the values according to your display size. In the format: [width, height]. You may need to experiment a bit with this.
+background_cmd   | If you're on Windows you can ignore this. If you're on Linux, this is the command that will change your background, scroll down to [notes for Linux users](#notes-for-linux-users) for more info.
+
+## Notes for Linux users:
+Since there are so many different ways to change the wallpaper in Linux depending on so many different factors, you'll probably get an error if you try to run the program as is. To fix this, use the internet to find how to change your wallpaper using the command line. Copy the command into the config and replace the path of the image in the command with `PIC`. 
+
+For example:
+
+In Gnome you change your wallpaper using the command `gsettings set org.gnome.desktop.background picture-uri /path/to/image.png`, so if you're a Gnome user the setting `background_cmd` should be `gsettings set org.gnme.desktop.background picture-uri PIC`.
+
+## Colors
+![Colors](colors.png)
---
+++ main.py
@@ -0,0 +1,167 @@
+import json
+import matplotlib.pyplot as plt
+import os
+import platform
+import requests
+import sys
+import time
+import traceback
+
+plat = platform.system()
+
+if plat == "Linux":
+    import subprocess
+elif plat == "Windows":
+    import ctypes
+else:
+    print("Currently unsupported OS, shouldn't be too easy to hard yourself though!")
+
+
+class SetBackground():
+    def __init__(self, conf_path, plat):
+        self.conf_path = conf_path
+
+        self.img_name = "chart.png"
+        self.last_price = -1
+
+        self.plat = plat
+
+    def set_wallpaper(self):
+        img_path = os.path.abspath(self.img_name)
+        if self.plat == "Windows":
+            ctypes.windll.user32.SystemParametersInfoW(0x14, 0, img_path, 0)
+
+        if self.plat == "Linux":
+            args = self.conf["background_cmd"].split()
+            for x, arg in enumerate(args):
+                if arg == "PIC":
+                    args[x] = img_path
+
+            if img_path not in args:
+                raise Exception(
+                    'Image path is not present in config, remember to have "PIC" as an argument'
+                )
+
+            try:
+                subprocess.check_call(args)
+            except subprocess.CalledProcessError as e:
+                raise Exception(
+                    f'{e}\nError when changing wallpaper, correctly modify "background_cmd" in the config.'
+                )
+
+    def reload_conf(self):
+        self.conf = json.loads(open(self.conf_path, "r").read())
+
+        self.coin, self.symbol, self.name = self.get_coin_info(
+            self.conf["coin"])
+
+        self.currency = self.conf["currency"].upper()
+        self.timespan = self.conf["timespan"]
+
+        if self.conf["dark_theme"]:
+            plt.style.use("dark_background")
+
+    def get_coin_info(self, coin):
+        r = requests.get(
+            f"https://api.coingecko.com/api/v3/coins/{coin}?localization=false&tickers=false&market_data=false&community_data=false&developer_data=false")
+
+        try:
+            res = json.loads(r.text)
+        except json.decoder.JSONDecodeError:
+            # print(f"Failed to decode response: {r.text}")
+            sys.exit()
+
+        symbol = res["symbol"]
+        name = res["name"]
+
+        return coin, symbol, name
+
+    def get_prices(self):
+        if self.timespan == "max" or self.timespan == 0:
+            r = requests.get(
+                f"https://api.coingecko.com/api/v3/coins/{self.coin}/market_chart?vs_currency={self.currency}&days=max")
+        else:
+            to_h = time.time()
+            from_h = to_h - (self.timespan * 3600)
+            r = requests.get(
+                f"https://api.coingecko.com/api/v3/coins/{self.coin}/market_chart/range?vs_currency={self.currency}&from={from_h}&to={to_h}")
+
+        try:
+            res = json.loads(r.text)
+        except json.decoder.JSONDecodeError:
+            # print(f"Failed to decode response: {r.text}")
+            return None
+
+        if "error" in res:
+            print(res["error"])
+            return None
+
+        return res["prices"]
+
+    def create_image(self, prices, curr_price):
+        self.last_price = curr_price
+
+        x = [x for x in range(len(prices))]
+        y = [x[1] for x in prices]
+
+        plt.figure(figsize=(self.conf["figsize"][0], self.conf["figsize"][1]))
+        plt.plot(x, y, color=self.conf["color"])
+
+        plt.ylabel(f"Price in {self.currency}")
+
+        title_str = f"Price chart for {self.name}\n1 {self.symbol.upper()} = {curr_price} {self.currency}\n"
+
+        highest = max(y)
+        lowest = min(y)
+
+        if highest > 10:
+            title_str += f"High: {highest:.2f} {self.currency}    "
+        else:
+            title_str += f"High: {highest:.8f} {self.currency}    "
+
+        if lowest > 10:
+            title_str += f"Low: {lowest:.2f} {self.currency}\n"
+        else:
+            title_str += f"Low: {lowest:.8f} {self.currency}\n"
+
+        plt.title(title_str)
+        plt.tick_params(bottom=False, labelbottom=False)
+
+        plt.savefig(self.img_name, dpi=300)
+        plt.close()
+
+    def loop(self):
+        while True:
+            self.reload_conf()
+
+            prices = self.get_prices()
+
+            try:
+                curr_price = prices[-1][-1]
+                if curr_price > 10:
+                    curr_price = format(curr_price, ".2f")
+                else:
+                    curr_price = format(curr_price, ".8f")
+            except TypeError:
+                curr_price = None
+
+            if prices != None and curr_price != None and self.last_price != curr_price:
+                print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(prices[-1][0]/1000)),
+                      "-", curr_price, self.currency)
+
+                self.create_image(prices, curr_price)
+                self.set_wallpaper()
+
+            time.sleep(self.conf["refresh_interval"])
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            SetBackground("config.json", plat).loop()
+        except requests.exceptions.ConnectionError:
+            print("No internet connection.")
+            time.sleep(5)
+        except Exception:
+            traceback.print_exc()
+            time.sleep(2)
